ec1509807385a208ee540bddb8be933077a30e39
Martin Sirringhaus <>
2020-11-10 09:07:00 +0100

    Adding unit tests for maps_reader

--- src/maps_reader.rs
+++ src/maps_reader.rs
@@ -11,7 +11,7 @@ pub const DELETED_SUFFIX: &'static str = " (deleted)";
 pub const MOZILLA_IPC_PREFIX: &'static str = "org.mozilla.ipc.";
 pub const RESERVED_FLAGS: &'static str = " ---p";
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct SystemMappingInfo {
     pub start_address: usize,
     pub end_address: usize,
@@ -19,7 +19,7 @@ pub struct SystemMappingInfo {
 
 // One of these is produced for each mapping in the process (i.e. line in
 // /proc/$x/maps).
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct MappingInfo {
     // On Android, relocation packing can mean that the reported start
     // address of the mapping must be adjusted by a bias in order to
@@ -218,5 +218,203 @@ impl MappingInfo {
             }
         }
         false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_lines_and_loc() -> (Vec<&'static str>, u64) {
+        (vec![
+"5597483fc000-5597483fe000 r--p 00000000 00:31 4750073                    /usr/bin/cat",
+"5597483fe000-559748402000 r-xp 00002000 00:31 4750073                    /usr/bin/cat",
+"559748402000-559748404000 r--p 00006000 00:31 4750073                    /usr/bin/cat",
+"559748404000-559748405000 r--p 00007000 00:31 4750073                    /usr/bin/cat",
+"559748405000-559748406000 rw-p 00008000 00:31 4750073                    /usr/bin/cat",
+"559749b0e000-559749b2f000 rw-p 00000000 00:00 0                          [heap]",
+"7efd968d3000-7efd968f5000 rw-p 00000000 00:00 0",
+"7efd968f5000-7efd9694a000 r--p 00000000 00:31 5004638                    /usr/lib/locale/en_US.utf8/LC_CTYPE",
+"7efd9694a000-7efd96bc2000 r--p 00000000 00:31 5004373                    /usr/lib/locale/en_US.utf8/LC_COLLATE",
+"7efd96bc2000-7efd96bc4000 rw-p 00000000 00:00 0",
+"7efd96bc4000-7efd96bea000 r--p 00000000 00:31 4996104                    /lib64/libc-2.32.so",
+"7efd96bea000-7efd96d39000 r-xp 00026000 00:31 4996104                    /lib64/libc-2.32.so",
+"7efd96d39000-7efd96d85000 r--p 00175000 00:31 4996104                    /lib64/libc-2.32.so",
+"7efd96d85000-7efd96d86000 ---p 001c1000 00:31 4996104                    /lib64/libc-2.32.so",
+"7efd96d86000-7efd96d89000 r--p 001c1000 00:31 4996104                    /lib64/libc-2.32.so",
+"7efd96d89000-7efd96d8c000 rw-p 001c4000 00:31 4996104                    /lib64/libc-2.32.so",
+"7efd96d8c000-7efd96d92000 rw-p 00000000 00:00 0",
+"7efd96da0000-7efd96da1000 r--p 00000000 00:31 5004379                    /usr/lib/locale/en_US.utf8/LC_NUMERIC",
+"7efd96da1000-7efd96da2000 r--p 00000000 00:31 5004382                    /usr/lib/locale/en_US.utf8/LC_TIME",
+"7efd96da2000-7efd96da3000 r--p 00000000 00:31 5004377                    /usr/lib/locale/en_US.utf8/LC_MONETARY",
+"7efd96da3000-7efd96da4000 r--p 00000000 00:31 5004376                    /usr/lib/locale/en_US.utf8/LC_MESSAGES/SYS_LC_MESSAGES",
+"7efd96da4000-7efd96da5000 r--p 00000000 00:31 5004380                    /usr/lib/locale/en_US.utf8/LC_PAPER",
+"7efd96da5000-7efd96da6000 r--p 00000000 00:31 5004378                    /usr/lib/locale/en_US.utf8/LC_NAME",
+"7efd96da6000-7efd96da7000 r--p 00000000 00:31 5004372                    /usr/lib/locale/en_US.utf8/LC_ADDRESS",
+"7efd96da7000-7efd96da8000 r--p 00000000 00:31 5004381                    /usr/lib/locale/en_US.utf8/LC_TELEPHONE",
+"7efd96da8000-7efd96da9000 r--p 00000000 00:31 5004375                    /usr/lib/locale/en_US.utf8/LC_MEASUREMENT",
+"7efd96da9000-7efd96db0000 r--s 00000000 00:31 5004639                    /usr/lib64/gconv/gconv-modules.cache",
+"7efd96db0000-7efd96db1000 r--p 00000000 00:31 5004374                    /usr/lib/locale/en_US.utf8/LC_IDENTIFICATION",
+"7efd96db1000-7efd96db2000 r--p 00000000 00:31 4996100                    /lib64/ld-2.32.so",
+"7efd96db2000-7efd96dd3000 r-xp 00001000 00:31 4996100                    /lib64/ld-2.32.so",
+"7efd96dd3000-7efd96ddc000 r--p 00022000 00:31 4996100                    /lib64/ld-2.32.so",
+"7efd96ddc000-7efd96ddd000 r--p 0002a000 00:31 4996100                    /lib64/ld-2.32.so",
+"7efd96ddd000-7efd96ddf000 rw-p 0002b000 00:31 4996100                    /lib64/ld-2.32.so",
+"7ffc6dfda000-7ffc6dffb000 rw-p 00000000 00:00 0                          [stack]",
+"7ffc6e0f3000-7ffc6e0f7000 r--p 00000000 00:00 0                          [vvar]",
+"7ffc6e0f7000-7ffc6e0f9000 r-xp 00000000 00:00 0                          [vdso]",
+"ffffffffff600000-ffffffffff601000 --xp 00000000 00:00 0                  [vsyscall]"
+        ], 0x7ffc6e0f7000)
+    }
+
+    fn get_all_mappings() -> Vec<MappingInfo> {
+        let mut mappings: Vec<MappingInfo> = Vec::new();
+        let (lines, linux_gate_loc) = get_lines_and_loc();
+        // Only /usr/bin/cat and [heap]
+        for line in lines {
+            match MappingInfo::parse_from_line(&line, linux_gate_loc, mappings.last_mut()) {
+                Ok(MappingInfoParsingResult::Success(map)) => mappings.push(map),
+                Ok(MappingInfoParsingResult::SkipLine) => continue,
+                Err(_) => assert!(false),
+            }
+        }
+        assert_eq!(mappings.len(), 23);
+        mappings
+    }
+
+    #[test]
+    fn test_merged() {
+        let mut mappings: Vec<MappingInfo> = Vec::new();
+        let (lines, linux_gate_loc) = get_lines_and_loc();
+        // Only /usr/bin/cat and [heap]
+        for line in lines[0..=6].iter() {
+            match MappingInfo::parse_from_line(&line, linux_gate_loc, mappings.last_mut()) {
+                Ok(MappingInfoParsingResult::Success(map)) => mappings.push(map),
+                Ok(MappingInfoParsingResult::SkipLine) => continue,
+                Err(_) => assert!(false),
+            }
+        }
+
+        assert_eq!(mappings.len(), 3);
+        let cat_map = MappingInfo {
+            start_address: 0x5597483fc000,
+            size: 40960,
+            system_mapping_info: SystemMappingInfo {
+                start_address: 0x5597483fc000,
+                end_address: 0x559748406000,
+            },
+            offset: 0,
+            executable: true,
+            name: Some("/usr/bin/cat".to_string()),
+        };
+
+        assert_eq!(mappings[0], cat_map);
+
+        let heap_map = MappingInfo {
+            start_address: 0x559749b0e000,
+            size: 135168,
+            system_mapping_info: SystemMappingInfo {
+                start_address: 0x559749b0e000,
+                end_address: 0x559749b2f000,
+            },
+            offset: 0,
+            executable: false,
+            name: Some("[heap]".to_string()),
+        };
+
+        assert_eq!(mappings[1], heap_map);
+
+        let empty_map = MappingInfo {
+            start_address: 0x7efd968d3000,
+            size: 139264,
+            system_mapping_info: SystemMappingInfo {
+                start_address: 0x7efd968d3000,
+                end_address: 0x7efd968f5000,
+            },
+            offset: 0,
+            executable: false,
+            name: None,
+        };
+
+        assert_eq!(mappings[2], empty_map);
+    }
+
+    #[test]
+    fn test_linux_gate_parsing() {
+        let mappings = get_all_mappings();
+
+        let gate_map = MappingInfo {
+            start_address: 0x7ffc6e0f7000,
+            size: 8192,
+            system_mapping_info: SystemMappingInfo {
+                start_address: 0x7ffc6e0f7000,
+                end_address: 0x7ffc6e0f9000,
+            },
+            offset: 0,
+            executable: true,
+            name: Some("linux-gate.so".to_string()),
+        };
+
+        assert_eq!(mappings[21], gate_map);
+    }
+
+    #[test]
+    fn test_reading_all() {
+        let mappings = get_all_mappings();
+
+        let found_items = vec![
+            Some("/usr/bin/cat".to_string()),
+            Some("[heap]".to_string()),
+            None,
+            Some("/usr/lib/locale/en_US.utf8/LC_CTYPE".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_COLLATE".to_string()),
+            None,
+            Some("/lib64/libc-2.32.so".to_string()),
+            // The original shows a None here, but this is an address ranges that the
+            // linker reserved but which a loaded library did not use. These
+            // appear as an anonymous private mapping with no access flags set
+            // and which directly follow an executable mapping.
+            Some("/usr/lib/locale/en_US.utf8/LC_NUMERIC".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_TIME".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_MONETARY".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_MESSAGES/SYS_LC_MESSAGES".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_PAPER".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_NAME".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_ADDRESS".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_TELEPHONE".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_MEASUREMENT".to_string()),
+            Some("/usr/lib64/gconv/gconv-modules.cache".to_string()),
+            Some("/usr/lib/locale/en_US.utf8/LC_IDENTIFICATION".to_string()),
+            Some("/lib64/ld-2.32.so".to_string()),
+            Some("[stack]".to_string()),
+            Some("[vvar]".to_string()),
+            // This is rewritten from [vdso] to linux-gate.so
+            Some("linux-gate.so".to_string()),
+            Some("[vsyscall]".to_string()),
+        ];
+
+        assert_eq!(
+            mappings.iter().map(|x| x.name.clone()).collect::<Vec<_>>(),
+            found_items
+        );
+    }
+
+    #[test]
+    fn test_merged_reserved_mappings() {
+        let mappings = get_all_mappings();
+
+        let gate_map = MappingInfo {
+            start_address: 0x7efd96bc4000,
+            size: 1892352, // Merged the anonymous area after in this mapping, so its bigger..
+            system_mapping_info: SystemMappingInfo {
+                start_address: 0x7efd96bc4000,
+                end_address: 0x7efd96d8c000, // ..but this is not visible here
+            },
+            offset: 0,
+            executable: true,
+            name: Some("/lib64/libc-2.32.so".to_string()),
+        };
+
+        assert_eq!(mappings[6], gate_map);
     }
 }
