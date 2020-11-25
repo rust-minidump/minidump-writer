f8c961a94b4ca5e5365714da29672a4d1450d008
Martin Sirringhaus <>
2020-11-25 13:33:57 +0100

    Make write_dump use temporary file and check that the dump actually gets written

--- tests/ptrace_dumper.rs
+++ tests/ptrace_dumper.rs
@@ -216,7 +216,13 @@ fn test_write_dump() {
     let num_of_threads = 3;
     let mut child = start_child_and_wait_for_threads(num_of_threads);
     let pid = child.id() as i32;
-    write_minidump("/tmp/testdump", pid, pid).expect("Could not write minidump");
+
+    let tmpfile = tempfile::Builder::new()
+        .prefix("write_dump")
+        .tempfile()
+        .unwrap();
+
+    write_minidump(tmpfile.path().to_str().unwrap(), pid, pid).expect("Could not write minidump");
     child.kill().expect("Failed to kill process");
 
     // Reap child
@@ -224,6 +230,9 @@ fn test_write_dump() {
     let status = waitres.signal().expect("Child did not die due to signal");
     assert_eq!(waitres.code(), None);
     assert_eq!(status, Signal::SIGKILL as i32);
+
+    let meta = std::fs::metadata(tmpfile.path()).expect("Couldn't get metadata for tempfile");
+    assert!(meta.len() > 0);
 }
 
 #[test]
