693a0228d9257607cc96a885d7f24bab4ecbea01
Martin Sirringhaus <>
2020-12-17 15:41:36 +0100

    Switch to using Default::default() when filling out MDExceptions

--- src/sections/exception_stream.rs
+++ src/sections/exception_stream.rs
@@ -5,50 +5,35 @@ use minidump_common::format::*;
 
 pub fn write(config: &mut MinidumpWriter, buffer: &mut DumpBuf) -> Result<MDRawDirectory> {
     let exception = if let Some(context) = &config.crash_context {
-        // TODO: Default::default()
-        MDException {
-            exception_code: context.siginfo.si_signo as u32,
-            exception_flags: context.siginfo.si_code as u32,
-            exception_record: 0,
-            exception_address: unsafe { context.siginfo.si_addr() } as u64,
-            number_parameters: 0,
-            __align: 0,
-            exception_information: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        }
+        let mut res: MDException = Default::default();
+        res.exception_code = context.siginfo.si_signo as u32;
+        res.exception_flags = context.siginfo.si_code as u32;
+        res.exception_address = unsafe { context.siginfo.si_addr() } as u64;
+        res
     } else {
         let addr = match config.crashing_thread_context {
             CrashingThreadContext::CrashContextPlusAddress((_, addr)) => addr,
             _ => 0,
         };
-        MDException {
-            exception_code: MD_EXCEPTION_CODE_LIN_DUMP_REQUESTED as u32,
-            exception_flags: 0,
-            exception_record: 0,
-            exception_address: addr,
-            number_parameters: 0,
-            __align: 0,
-            exception_information: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        }
+        let mut res: MDException = Default::default();
+        res.exception_code = MD_EXCEPTION_CODE_LIN_DUMP_REQUESTED;
+        res.exception_address = addr;
+        res
     };
 
     let thread_context = match config.crashing_thread_context {
         CrashingThreadContext::CrashContextPlusAddress((ctx, _)) => ctx,
         CrashingThreadContext::CrashContext(ctx) => ctx,
-        CrashingThreadContext::None => MDLocationDescriptor {
-            data_size: 0,
-            rva: 0,
-        },
+        CrashingThreadContext::None => Default::default(),
     };
 
-    let stream = MDRawExceptionStream {
-        thread_id: config.blamed_thread as u32,
-        exception_record: exception,
-        __align: 0,
-        thread_context,
-    };
+    let mut stream: MDRawExceptionStream = Default::default();
+    stream.thread_id = config.blamed_thread as u32;
+    stream.exception_record = exception;
+    stream.thread_context = thread_context;
     let exc = MemoryWriter::alloc_with_val(buffer, stream)?;
     let dirent = MDRawDirectory {
-        stream_type: MD_EXCEPTION_STREAM as u32,
+        stream_type: MD_EXCEPTION_STREAM,
         location: exc.location(),
     };
 
