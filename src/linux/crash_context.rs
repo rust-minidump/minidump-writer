7695c64b59e1d99f2ce1840586a6c43f79e0af2b
Jake Shadle <jake.shadle@embark-studios.com>
2021-11-19 06:41:45 +0100

    Fixup crash_context

--- src/linux/crash_context.rs
+++ src/linux/crash_context.rs
@@ -4,34 +4,19 @@
 
 cfg_if::cfg_if! {
     if #[cfg(target_arch = "x86_64")] {
-        mod crash_context_x86_64;
+        mod x86_64;
     } else if #[cfg(target_arch = "x86")] {
-        mod crash_context_x86;
+        mod x86;
     } else if #[cfg(target_arch = "aarch64")] {
-        mod crash_context_aarch64;
+        mod aarch64;
     } else if #[cfg(target_arch = "arm")] {
-        mod crash_context_arm;
+        mod arm;
     } else if #[cfg(target_arch = "mips")] {
-        mod crash_context_mips;
+        mod mips;
     }
 }
 
 use crate::minidump_cpu::RawContextCPU;
-
-// #[cfg(target_arch = "aarch64")]
-// pub type fpstate_t = libc::fpsimd_context; // Currently not part of libc! This will produce an error.
-// #[cfg(not(any(
-//     target_arch = "aarch64",
-//     target_arch = "mips",
-//     target_arch = "arm-eabi"
-// )))]
-// #[cfg(target_arch = "x86")]
-// #[allow(non_camel_case_types)]
-// pub type fpstate_t = libc::_libc_fpstate;
-// #[cfg(target_arch = "x86_64")]
-// #[allow(non_camel_case_types)]
-// pub type fpstate_t = libc::user_fpregs_struct;
-
 pub use exception_handler::CrashContext;
 
 pub trait CpuContext {
