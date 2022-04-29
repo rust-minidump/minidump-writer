a56338864485ce2037d0132d542d22c6d8d15ab1
Jake Shadle <jake.shadle@embark-studios.com>
2022-04-29 07:47:17 +0200

    Fix windows test compilation

--- tests/windows_minidump_writer.rs
+++ tests/windows_minidump_writer.rs
@@ -56,7 +56,7 @@ fn dump_current_process() {
             exception_code: STATUS_INVALID_PARAMETER,
         };
 
-        let dumper = MinidumpWriter::new(crash_context);
+        let dumper = MinidumpWriter::new(crash_context).expect("failed to create MinidumpWriter");
 
         dumper
             .dump(tmpfile.as_file_mut())
