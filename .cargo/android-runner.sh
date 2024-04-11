ba410b7060dd05980f4e82378170938455e8003a
Alex Franchuk <afranchuk@mozilla.com>
2024-04-11 10:15:43 -0400

    Add aarch64-android testing to CI.

--- .cargo/android-runner.sh
+++ .cargo/android-runner.sh
@@ -3,7 +3,12 @@
 BINARY=$1
 shift
 
+# When pushing a bin, the full target dir is appended (including the target
+# triple). There's no need for this, so strip it away. This simplifies the
+# `TEST_HELPER` definition across multiple architectures.
+REMOTE_BINARY="/data/local/$(basename $BINARY)"
+
 # Make sure to run the following to copy the test helper binary over.
-# cargo run --target x86_64-linux-android --bin test
-adb push "$BINARY" "/data/local/$BINARY"
-adb shell "chmod 777 /data/local/$BINARY && env TEST_HELPER=/data/local/target/x86_64-linux-android/debug/test /data/local/$BINARY" "$@"
+# cargo run --target ANDROID-TARGET --bin test
+adb push $BINARY $REMOTE_BINARY
+adb shell "chmod 777 $REMOTE_BINARY && env TEST_HELPER=/data/local/test $REMOTE_BINARY" "$@"
