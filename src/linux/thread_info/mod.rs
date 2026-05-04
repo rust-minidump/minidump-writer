08da9ae13f18037512ae86f3a66e5983a82648dd
Chris Martin <marti4d@live.ca>
2026-05-04 12:09:12 -0400

    Isolated Processes 1: Refactor thread_info for ease of redirecting
    Currently, thread_info is a bit of a mess and is difficult to cleanly
    redirect into an interface.
    
    This cleans it up and makes it easier to move around.
    
    It also fixes a piece of unsound code (mod.rs line 165) where we are
    passing an immutable pointer to an API that says it will modify the
    result.

--- src/linux/thread_info/mod.rs
+++ src/linux/thread_info/mod.rs
@@ -1,12 +1,16 @@
 use {
     super::{Pid, serializers::*},
     crate::serializers::*,
-    nix::{errno::Errno, sys::ptrace},
+    nix::errno::Errno,
     std::{
+        ffi::c_void,
         io::{self, BufRead},
         path,
     },
 };
+
+#[cfg(target_arch = "x86_64")]
+pub use x86::copy_u32_registers;
 
 type Result<T> = std::result::Result<T, ThreadInfoError>;
 
@@ -30,7 +34,7 @@ pub enum ThreadInfoError {
     ),
     #[error("nix::ptrace() error")]
     PtraceError(
-        #[from]
+        #[source]
         #[serde(serialize_with = "serialize_nix_error")]
         nix::Error,
     ),
@@ -51,141 +55,79 @@ cfg_if::cfg_if! {
     }
 }
 
-#[derive(Debug)]
-#[allow(non_camel_case_types, dead_code)]
-enum NT_Elf {
-    NT_NONE = 0,
-    NT_PRSTATUS = 1,
-    NT_PRFPREGSET = 2,
-    //NT_PRPSINFO = 3,
-    //NT_TASKSTRUCT = 4,
-    //NT_AUXV = 6,
-    NT_ARM_VFP = 0x400, // ARM VFP/NEON registers
-}
+#[cfg(target_env = "gnu")]
+type PtraceRequestType = core::ffi::c_uint;
 
-#[cfg(target_arch = "x86_64")]
-#[inline]
-pub fn copy_u32_registers(dst: &mut [u128], src: &[u32]) {
-    // SAFETY: We are copying a block of memory from ptrace as u32s to the u128
-    // format of minidump-common
-    unsafe {
-        let dst: &mut [u8] =
-            std::slice::from_raw_parts_mut(dst.as_mut_ptr().cast(), dst.len() * 16);
-        let src: &[u8] = std::slice::from_raw_parts(src.as_ptr().cast(), src.len() * 4);
+#[cfg(not(target_env = "gnu"))]
+type PtraceRequestType = core::ffi::c_int;
 
-        let to_copy = std::cmp::min(dst.len(), src.len());
-        dst[..to_copy].copy_from_slice(&src[..to_copy]);
-    }
-}
+fn get_ppid_and_tgid(tid: Pid) -> Result<(Pid, Pid)> {
+    let mut ppid = -1;
+    let mut tgid = -1;
 
-trait CommonThreadInfo {
-    fn get_ppid_and_tgid(tid: Pid) -> Result<(Pid, Pid)> {
-        let mut ppid = -1;
-        let mut tgid = -1;
-
-        let status_path = path::PathBuf::from(format!("/proc/{tid}/status"));
-        let status_file = std::fs::File::open(status_path)?;
-        for line in io::BufReader::new(status_file).lines() {
-            let l = line?;
-            let start = l
-                .get(0..6)
-                .ok_or_else(|| ThreadInfoError::InvalidProcStatusFile(tid, l.clone()))?;
-            match start {
-                "Tgid:\t" => {
-                    tgid = l
-                        .get(6..)
-                        .ok_or_else(|| ThreadInfoError::InvalidProcStatusFile(tid, l.clone()))?
-                        .parse::<Pid>()?;
-                }
-                "PPid:\t" => {
-                    ppid = l
-                        .get(6..)
-                        .ok_or_else(|| ThreadInfoError::InvalidProcStatusFile(tid, l.clone()))?
-                        .parse::<Pid>()?;
-                }
-                _ => continue,
+    let status_path = path::PathBuf::from(format!("/proc/{tid}/status"));
+    let status_file = std::fs::File::open(status_path)?;
+    for line in io::BufReader::new(status_file).lines() {
+        let l = line?;
+        let start = l
+            .get(0..6)
+            .ok_or_else(|| ThreadInfoError::InvalidProcStatusFile(tid, l.clone()))?;
+        match start {
+            "Tgid:\t" => {
+                tgid = l
+                    .get(6..)
+                    .ok_or_else(|| ThreadInfoError::InvalidProcStatusFile(tid, l.clone()))?
+                    .parse::<Pid>()?;
             }
-        }
-        if ppid == -1 || tgid == -1 {
-            return Err(ThreadInfoError::InvalidPid(
-                format!("/proc/{tid}/status"),
-                ppid,
-                tgid,
-            ));
-        }
-        Ok((ppid, tgid))
-    }
-
-    /// SLIGHTLY MODIFIED COPY FROM CRATE nix
-    /// Function for ptrace requests that return values from the data field.
-    /// Some ptrace get requests populate structs or larger elements than `c_long`
-    /// and therefore use the data field to return values. This function handles these
-    /// requests.
-    fn ptrace_get_data<T>(
-        request: ptrace::RequestType,
-        flag: Option<NT_Elf>,
-        pid: nix::unistd::Pid,
-    ) -> Result<T> {
-        let mut data = std::mem::MaybeUninit::uninit();
-        let res = unsafe {
-            libc::ptrace(
-                request,
-                libc::pid_t::from(pid),
-                flag.unwrap_or(NT_Elf::NT_NONE),
-                data.as_mut_ptr(),
-            )
-        };
-        Errno::result(res)?;
-        Ok(unsafe { data.assume_init() })
-    }
-
-    /// SLIGHTLY MODIFIED COPY FROM CRATE nix
-    /// Function for ptrace requests that return values from the data field.
-    /// Some ptrace get requests populate structs or larger elements than `c_long`
-    /// and therefore use the data field to return values. This function handles these
-    /// requests.
-    fn ptrace_get_data_via_io<T>(
-        request: ptrace::RequestType,
-        flag: Option<NT_Elf>,
-        pid: nix::unistd::Pid,
-    ) -> Result<T> {
-        let mut data = std::mem::MaybeUninit::<T>::uninit();
-        let io = libc::iovec {
-            iov_base: data.as_mut_ptr().cast(),
-            iov_len: std::mem::size_of::<T>(),
-        };
-        let res = unsafe {
-            libc::ptrace(
-                request,
-                libc::pid_t::from(pid),
-                flag.unwrap_or(NT_Elf::NT_NONE),
-                &io as *const _,
-            )
-        };
-        Errno::result(res)?;
-        Ok(unsafe { data.assume_init() })
-    }
-
-    /// COPY FROM CRATE nix BECAUSE ITS NOT PUBLIC
-    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-    fn ptrace_peek(
-        request: ptrace::RequestType,
-        pid: nix::unistd::Pid,
-        addr: ptrace::AddressType,
-        data: *mut libc::c_void,
-    ) -> nix::Result<libc::c_long> {
-        let ret = unsafe {
-            Errno::clear();
-            libc::ptrace(request, libc::pid_t::from(pid), addr, data)
-        };
-        match Errno::result(ret) {
-            Ok(..) | Err(Errno::UnknownErrno) => Ok(ret),
-            err @ Err(..) => err,
+            "PPid:\t" => {
+                ppid = l
+                    .get(6..)
+                    .ok_or_else(|| ThreadInfoError::InvalidProcStatusFile(tid, l.clone()))?
+                    .parse::<Pid>()?;
+            }
+            _ => continue,
         }
     }
+    if ppid == -1 || tgid == -1 {
+        return Err(ThreadInfoError::InvalidPid(
+            format!("/proc/{tid}/status"),
+            ppid,
+            tgid,
+        ));
+    }
+    Ok((ppid, tgid))
 }
-impl ThreadInfo {
-    pub fn create(pid: Pid, tid: Pid) -> std::result::Result<Self, ThreadInfoError> {
-        Self::create_impl(pid, tid)
+
+/// Safety: RequestType and T must agree on the size of the returned type
+unsafe fn ptrace_getregs<T>(request: PtraceRequestType, pid: libc::pid_t) -> Result<T> {
+    let mut output = std::mem::MaybeUninit::uninit();
+
+    // Since ptrace() is vararg, best to explicitly state arg types
+    let addr: *mut c_void = std::ptr::null_mut();
+    let data: *mut c_void = (&raw mut output).cast();
+    let res = unsafe { libc::ptrace(request, pid, addr, data) };
+    Errno::result(res).map_err(ThreadInfoError::PtraceError)?;
+    Ok(unsafe { output.assume_init() })
+}
+
+fn ptrace_getregset<T>(regset_type: usize, pid: libc::pid_t) -> Result<T> {
+    let mut output = std::mem::MaybeUninit::<T>::uninit();
+    let mut io = libc::iovec {
+        iov_base: output.as_mut_ptr().cast(),
+        iov_len: std::mem::size_of::<T>(),
+    };
+
+    // Since ptrace() is vararg, best to explicitly state arg types
+    let addr: *mut c_void = regset_type as *mut c_void;
+    let data: *mut c_void = (&raw mut io).cast();
+    let res = unsafe { libc::ptrace(libc::PTRACE_GETREGSET, pid, addr, data) };
+    Errno::result(res).map_err(ThreadInfoError::PtraceError)?;
+
+    // PTRACE_GETREGSET returns the number of bytes actually read in iov_len. Need to ensure
+    // all bytes of T are actually initialized
+    if io.iov_len != std::mem::size_of::<T>() {
+        return Err(ThreadInfoError::PtraceError(Errno::EINVAL));
     }
+
+    Ok(unsafe { output.assume_init() })
 }
