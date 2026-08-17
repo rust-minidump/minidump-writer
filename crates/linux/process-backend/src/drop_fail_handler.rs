5857d3a083f655c48b1d3ff03fc689cc18377414
Chris Martin <marti4d@live.ca>
2026-08-17 11:12:44 -0400

    Address review feedback from Gabriele

--- crates/linux/process-backend/src/drop_fail_handler.rs
+++ crates/linux/process-backend/src/drop_fail_handler.rs
@@ -1,3 +1,22 @@
+//! Set a custom handler for when a Drop failure occurs
+//!
+//! Normally, there is not much that can be done when an error occurs during execution of `drop()`
+//! -- Common strategies are to just ignore it, or to just log it and ignore it.
+//!
+//! We prefer the latter approach, but the issue is that if a Rust global logger has been set up
+//! in the crashed process that will run the Executor, that could result in `log` being used in
+//! a signal handler context. And since common logging behavior is to print to stderr, that will
+//! almost-certainly cause a deadlock or some kind of other re-entrancy issue.
+//!
+//! Instead, we don't prescribe a behavior - We allow the user of minidump-writer to set their
+//! own handler for failed `drop()` calls. If the user knows it's safe to log or print or
+//! whatever, they can do that.
+//!
+//! Note that using [`install_global()`] to install a new handler will always overwrite any
+//! previously-installed handler. This is intentional, as the user of this crate may want to
+//! disable a previously-installed handler after a crash occurs (if that handler would cause
+//! re-entrancy issues)
+
 use core::sync::atomic::{AtomicPtr, Ordering};
 
 static HANDLER: AtomicPtr<Handler> = AtomicPtr::new(core::ptr::null_mut());
@@ -7,9 +26,13 @@ pub struct Handler {
 }
 
 impl Handler {
+    /// Decorate the given function as a potential global Drop handler
     pub const fn new(f: for<'a> fn(core::fmt::Arguments<'a>)) -> Self {
         Self { f }
     }
+    /// Install this handler as the new global Drop handler
+    ///
+    /// Will overwrite any previously installed handler
     pub fn install_global(&'static self) {
         HANDLER.store((self as *const Handler).cast_mut(), Ordering::Release);
     }
